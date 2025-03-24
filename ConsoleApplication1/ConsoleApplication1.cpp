833ac5dc4035df2e38334957296c255f7b2917fa
Do0vm <125159238+Do0vm@users.noreply.github.com>
2025-03-24 17:17:29 +0200

    [Add] +3Functions + Made Code More Readable + Fix minor mistakes

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,10 +1,8 @@
-// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
 #include <string>
-#include <cstdlib> // For rand() and srand()
-#include <ctime>   // For time()
+#include <cstdlib> 
+#include <ctime>   
 #include <vector>
 #include <map>
 
@@ -16,155 +14,127 @@ using std::rand;
 using std::vector;
 using std::map;
 
-
-void jumpLine()
-{
-
+void jumpLine() {
     cout << endl;
 }
 
-void Fighting()
-{
-    cout << "Slash" << endl;
+
+void Fighting() {
+    cout << "*Slash*" << endl;
+    
+    void jumpLine();
+    cout << "You gained Strength Skill" << endl;
+}
+
+int FatStackOfCashGenerator(int* Dollas) {
+   
+    *Dollas += ((rand() % 7)) + 1 * 99;
+    return *Dollas;
+}
+
+int main() {
+    srand(time(NULL));
 
   
-
-    cout << "You gained Skill1" << endl;
-    
-}
-
-
-int FatStackOfCashGenerator()
-{
-
-
-}
-
-int main()
-{
-    srand(time(NULL)); // Seed random number generator
-
     string Name;
-    string KingsName = "Jerome"; // Removed extra spaces
+    string KingsName = "Jerome"; // King's name
 
+    
     vector<string> weaponList{ "Knife", "Fist", "Gun" };
+
     
     int StrengthDice = ((rand() % 6) + 1) * 3;
     int MagicDice = ((rand() % 6) + 1) * 3;
+
+    
     int stamina = 6;
+    int PlayerMoney = 100;
 
-
-    map<string, int> abilities
-    {
-        { "Luck", 2},
+    
+    map<string, int> abilities{
+        { "Luck", 2 },
         { "MaxMana", 5 },
-        { "MaxStamina", 4 },
-
+        { "MaxStamina", 4 }
     };
 
+   
     cout << "Enter your name: ";
     cin >> Name;
-
     cout << "Hello " << Name << "!" << endl;
     cout << "My name is " << KingsName << "." << endl;
-
     cout << "Your strength is " << StrengthDice << endl;
     cout << "Your magic is " << MagicDice << endl;
 
-    cout << "Your bag contains: "  << endl;
-
-
-    for (int i = 0; i < weaponList.size(); i++)
-    {
-        cout << "\t-" << weaponList[i] << endl;
-
-
+   
+    cout << "Your bag contains: " << endl;
+    for (size_t i = 0; i < weaponList.size(); i++) {
+        cout << "\t- " << weaponList[i] << endl;
     }
+
     jumpLine();
-    cout << "Here is another fpr you" << endl;
+    cout << "Here is another for you" << endl;
 
+  
     weaponList.push_back("Potion");
+    cout << "You received a " << weaponList[3] << ". Your bag now contains:" << endl;
 
-    cout << "You recieved a " << weaponList[3]<< " Your bag now contains" << endl;
-
-    for (const string& weapon : weaponList)
-    {
-        cout << "\t- " << weapon << endl; 
+    // Print updated weapon list.
+    for (const string& weapon : weaponList) {
+        cout << "\t- " << weapon << endl;
     }
 
-
-
-
-
-    cout << "Your ability Luck" << abilities["Luck"] << endl;
-
-    
-
+    // Display abilities.
+    cout << "Your ability 'Luck': " << abilities["Luck"] << endl;
     cout << "Your Max Mana is " << abilities["MaxMana"] << endl;
     cout << "Your Max Stamina is " << abilities["MaxStamina"] << endl;
     jumpLine();
 
+    // Choose player class.
     cout << "Choose Your Class" << endl;
     cout << "1 is mage" << endl;
     cout << "2 is warrior" << endl;
-    cout << "3 is Berserke" << endl;
-    cout << "anything more is a peasent" << endl;
+    cout << "3 is berserker" << endl;
+    cout << "anything more is a peasant" << endl;
 
-
-    int gameclass= 0;
-    
+    int gameclass = 0;
     cin >> gameclass;
 
-    switch (gameclass)
-    {
+    switch (gameclass) {
     case 1:
         cout << "You are a mage" << endl;
         break;
-
     case 2:
-        cout << "You are a Warrior" << endl;
+        cout << "You are a warrior" << endl;
         break;
-
     case 3:
-        cout << "Berseker" << endl;
+        cout << "Berserker" << endl;
         break;
-
     default:
-
-        cout << "You are a peasent" << endl;
+        cout << "You are a peasant" << endl;
         break;
     }
 
-
+    
     cout << "You enter the courtyard to practice" << endl;
     jumpLine();
     cout << "You approach a training dummy" << endl;
 
-    do
-    {
-
-        
+    
+    do {
         Fighting();
-
-        StrengthDice ++;
-
-        stamina - 1;
-
+        StrengthDice++; 
+        stamina --;     
     } while (StrengthDice < 10);
-   
 
+    jumpLine();
+    jumpLine();
+    jumpLine();
 
-    cout << "You skill is now "<< StrengthDice << endl;
+    cout << "Your Strength skill is now " << StrengthDice << endl;
+    cout << "You notice something on the floor and pick it up" << endl;
 
+    FatStackOfCashGenerator(&PlayerMoney);
+    cout << "You collected somebody's money from the floor! Your money is now: " << PlayerMoney << endl;
 
-
-
-
-
-
-
-
-    return 0; // Explicit return statement
+    return 0; // Explicit return statement for clarity.
 }
-
-
